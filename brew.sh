67824100ada1c14e1c438a6ae731e2559639f1d9
Sarath C <sarath.c@experionglobal.com>
2016-08-18 22:41:03 +0530

    added brew utils

---
+++ brew.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+brew update
+brew upgrade
+
+brew install coreutils
+brew install moreutils
+brew install findutils
+brew install gnu-sed --with-default-names
+
+brew install zsh
+brew install bash
+brew tap homebrew/versions
+brew install bash-completion2
+
+brew install vim --override-system-vi
+brew install homebrew/dupes/grep
+brew install homebrew/dupes/openssh
+brew install homebrew/dupes/screen
+
+brew install openssl
+brew install tree
+brew install ffmpeg --with-libvpx
+brew install terminal-notifier
+
+brew install gh
+brew install node
+brew install git
+brew install git-town
+
+brew install heroku-toolbelt
+brew install mongodb
+brew install rbenv
+brew install fish
+
